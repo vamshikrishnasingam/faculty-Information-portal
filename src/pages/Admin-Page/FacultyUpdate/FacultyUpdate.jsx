5bb59ffa253132f2a5fbd356e411305cc89b9402
charanshashi <charanshashi029@gmail.com>
2023-12-04 01:30:42 +0530

    commit_4/12/23_1:30_charanshashi

--- src/pages/Admin-Page/FacultyUpdate/FacultyUpdate.jsx
+++ src/pages/Admin-Page/FacultyUpdate/FacultyUpdate.jsx
@@ -420,7 +420,7 @@ const ContactUS = () => {
                                 }`,
                               }}
                             >
-                              {facdata[day] && facdata[day][ele]
+                              {(facdata?.[day]?.[ele] || facdata?.['special']?.[day]?.[ele])
                                 ? "Busy"
                                 : "Idle"}
                               {ewlv === 1 && (
